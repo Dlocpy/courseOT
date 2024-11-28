cb8a102957a2d9f3a9fe40b271544c7012d25ef5
Dmitriy <dimanger@mail.ru>
2024-11-28 21:25:03 +0300

    Change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "lib.h"
 
+
 #include <iostream>
 
-int main (int, char **) {
+int main (int, char **) 
+{
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+ 
+   std::cout << "Hello, world!" << std::endl;
+  
+  return 0;
 
-    
+  
+  
 }
